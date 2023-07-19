7ead82b4450e85fa19ffec382a0454f207bd3b37
rpc <rpc7863@gamil.com>
2023-07-19 20:41:02 +0530

    updated

--- studentapi/settings.py
+++ studentapi/settings.py
@@ -79,11 +79,15 @@ WSGI_APPLICATION = 'studentapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',   # update this too
+        'URL': os.getenv("URL"),
+        'NAME': os.getenv("NAME"),
+        'USER': os.getenv("USER"),
+        'PASSWORD': os.getenv("PASSWORD"),
+        'HOST': os.getenv("HOST"),
+        'PORT': os.getenv("PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
