8da6fee5bbf04a91aaa1f836267a9ced1a7a37b7
rpc <rpc7863@gamil.com>
2023-07-21 14:26:13 +0530

    delete method override

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from core.models import Student
+from rest_framework import status
+from rest_framework.response import Response
 from core.serializers import StudentSerializer
 # Create your views here.
 
@@ -8,3 +10,8 @@ from core.serializers import StudentSerializer
 class StudentView(ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        student = self.get_object()
+        self.perform_destroy(instance=student)
+        return Response({"msg": f"student with name {student.name} delete successfully"}, status=status.HTTP_204_NO_CONTENT)
