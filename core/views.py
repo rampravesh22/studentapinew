54e48f179980c669e7b6335607e5b377cb16ce7d
rpc <rpc7863@gamil.com>
2023-07-21 14:48:19 +0530

    updated destroy method

--- core/views.py
+++ core/views.py
@@ -7,11 +7,18 @@ from core.serializers import StudentSerializer
 # Create your views here.
 
 
-class StudentView(ModelViewSet):
+class StudentView(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-    # def destroy(self, request, *args, **kwargs):
-    #     student = self.get_object()
-    #     self.perform_destroy(instance=student)
-    #     return Response({"msg": f"student with name {student.name} delete successfully"}, status=status.HTTP_204_NO_CONTENT)
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        # Add your custom logic here before calling the base destroy method,
+        # if needed. For example, you can check permissions, log actions, etc.
+
+        # Call the base destroy method to delete the instance.
+        self.perform_destroy(instance)
+
+        # Return a custom response indicating successful deletion.
+        return Response({"message": "Student deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
