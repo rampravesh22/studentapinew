1b58431afca803f7909fc3033bfc7a19f5395472
rpc <rpc7863@gamil.com>
2023-07-21 14:41:02 +0530

    removed delete method

--- core/views.py
+++ core/views.py
@@ -11,7 +11,7 @@ class StudentView(ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-    def destroy(self, request, *args, **kwargs):
-        student = self.get_object()
-        self.perform_destroy(instance=student)
-        return Response({"msg": f"student with name {student.name} delete successfully"}, status=status.HTTP_204_NO_CONTENT)
+    # def destroy(self, request, *args, **kwargs):
+    #     student = self.get_object()
+    #     self.perform_destroy(instance=student)
+    #     return Response({"msg": f"student with name {student.name} delete successfully"}, status=status.HTTP_204_NO_CONTENT)
